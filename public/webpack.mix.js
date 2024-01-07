43e34658a7f6906e9be523bf63832a744a2e5cb7
Mr.Visca <62088810+mrvisca@users.noreply.github.com>
2024-01-07 17:37:56 +0700

    change thema

--- public/webpack.mix.js
+++ public/webpack.mix.js
@@ -1,10 +1,4 @@
 const mix = require("laravel-mix");
-const tailwindcss = require("tailwindcss");
-const { styles } = require("@ckeditor/ckeditor5-dev-utils");
-const CKERegex = {
-    svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-    css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
-};
 
 /*
  |--------------------------------------------------------------------------
@@ -17,71 +11,16 @@ const CKERegex = {
  |
  */
 
-Mix.listen("configReady", (webpackConfig) => {
-    const rules = webpackConfig.module.rules;
-    const targetSVG = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/;
-    const targetFont = /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/;
-    const targetCSS = /\.p?css$/;
-
-    for (let rule of rules) {
-        if (rule.test.toString() === targetSVG.toString()) {
-            rule.exclude = CKERegex.svg;
-        } else if (rule.test.toString() === targetFont.toString()) {
-            rule.exclude = CKERegex.svg;
-        } else if (rule.test.toString() === targetCSS.toString()) {
-            rule.exclude = CKERegex.css;
-        }
-    }
-});
-
 mix.js("src/js/app.js", "dist/js")
-    .sass("src/sass/app.scss", "dist/css")
+    .js("src/js/ckeditor-classic.js", "dist/js")
+    .js("src/js/ckeditor-inline.js", "dist/js")
+    .js("src/js/ckeditor-balloon.js", "dist/js")
+    .js("src/js/ckeditor-balloon-block.js", "dist/js")
+    .js("src/js/ckeditor-document.js", "dist/js")
+    .css("dist/css/_app.css", "dist/css/app.css")
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss("./tailwind.config.js")],
     })
-    .autoload({
-        "cash-dom": ["cash"],
-    })
-    .webpackConfig({
-        module: {
-            rules: [
-                {
-                    test: CKERegex.svg,
-                    use: ["raw-loader"],
-                },
-                {
-                    test: CKERegex.css,
-                    use: [
-                        {
-                            loader: "style-loader",
-                            options: {
-                                injectType: "singletonStyleTag",
-                                attributes: {
-                                    "data-cke": true,
-                                },
-                            },
-                        },
-                        "css-loader",
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                postcssOptions: styles.getPostCssConfig({
-                                    themeImporter: {
-                                        themePath: require.resolve(
-                                            "@ckeditor/ckeditor5-theme-lark"
-                                        ),
-                                    },
-                                    minify: true,
-                                }),
-                            },
-                        },
-                    ],
-                },
-            ],
-        },
-    })
-    .browserSync({
-        proxy: "icewall-html.test",
-        files: ["src/**/*.*"],
-    });
+    .copyDirectory("src/json", "dist/json")
+    .copyDirectory("src/fonts", "dist/fonts")
+    .copyDirectory("src/images", "dist/images");
